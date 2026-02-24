c0772a9e3f3db97462453e3f2a888a4853eb4463
graxyzr <greiceeszz@gmail.com>
2026-02-24 14:33:48 -0300

    Atualiza o HTML para usar o idioma correto e melhora a configuração do Vercel e do Vite para otimizar a estrutura do projeto

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    emptyOutDir: true
   }
 })
