14b648d709de1d46ae6e2b5d46a0712fc00a0b11
graxyzr <greiceeszz@gmail.com>
2026-02-24 14:10:30 -0300

    Restaura a URL do proxy CORS para garantir a funcionalidade correta do serviço freeToGame

--- src/services/freeToGameService.js
+++ src/services/freeToGameService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Proxy CORS público que funciona em produção
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/';
 const BASE_URL = 'https://www.freetogame.com/api';
 
 export const getGames = async () => {
